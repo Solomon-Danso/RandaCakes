362c5939a22bb9b30c30441b79d74f0decea9e7e
Solomon Danso <solomondanso2023@gmail.com>
2025-06-24 23:02:12 +0000

    Api Again

--- Constants/data.js
+++ Constants/data.js
@@ -1,5 +1,5 @@
-export const apiServer = "https://ugmallapi.hydottech.com/api/";
-export const apiMedia = "https://ugmallapi.hydottech.com/"; //Live 
+export const apiServer = "https://democommerceapi.hydottech.com/api/";
+export const apiMedia = "https://democommerceapi.hydottech.com/"; //Live 
 export const apiAInML = "https://ainmlserverapi.hydottech.com/"
  
 
